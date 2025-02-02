27d16cf8341d20966c9aa71d68c32b5a62795e2d
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2025-02-03 00:19:35 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className={'bg-ember-500'}>Hello world
+
+            <span className={'test'}>hello?</span>
+        </div>
+    </>
+  )
+}
+
+export default App
